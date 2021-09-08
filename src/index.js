adc93c0d7dd29ea6a273fd1f3d2405c87cbfbe68
Artem Deynega <artemdeynega892@gmail.com>
2021-09-09 01:20:25 +0300

    praktik-1

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import paintings from './paintings.json';
-import App from './App';
+import { App } from 'components/App';
+import './index.css';
 
-// const elem1 = <span>Привет</span>;
-
-// // React.createElement("span", { children: "мир" });
-
-// const elem2 = <span>мир</span>;
-
-// const element = (
-//   <div>
-//     {elem1}
-//     {elem2}
-//   </div>
-// );
-
-// console.log(element);
-
-const data = paintings[2];
 ReactDOM.render(<App />, document.getElementById('root'));
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
